f0a54c701ac2792ad60028b2e2833a5f565247ea
Qosay yassin <Qosay yassin@DESKTOP-D80LGBH>
2017-03-19 00:24:21 +0100

    التحويلات

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -10,8 +10,20 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            //like it
-            Console.WriteLine("Hello world");
+            char Var1 = 'A';
+            int Myvar = 'A';
+            char var2 = 'a';
+            int Myvar2 = 'a';
+            string vuleA = "this my vule";
+            string valea = "this is my valu a";
+            Console.WriteLine("\n vule A is = 0 ", Myvar);
+            Console.WriteLine("\n vule a is = 5", Myvar2);
+            char lol = Convert.ToChar(97);
+            Console.WriteLine("\n this my vule A {0} this is my vule a {1} my lol vule {2} ", Myvar, Myvar2,lol);
+
+            
+
+
         }
     }
 }
